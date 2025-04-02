97bd1fc76f12d04f7063a3eba926c80d234ed545
Sivaprakash Sivakumar <168542727+sivaprakashs33@users.noreply.github.com>
2025-04-02 17:23:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ require ('dotenv').config();
 const app = express();
 const cors = require("cors");
 
-
+app.use(cors({
+    origin: 'http://localhost:5173',  
+    methods: ['GET', 'POST','PUT','DELETE'],        
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  }));
 
 const dashboardRouter = require("./Router/userRouter");
 app.use(express.json());
-app.use(cors());
+
 
 app.use('/curd',dashboardRouter);
 
